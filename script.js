36c2e16bdbe1cff0d43fb1e5366864ebd4a3bb8f
Roger Qiu <84350865+rogerqiu7@users.noreply.github.com>
2022-05-11 08:21:17 -0500

    age update

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-// <span id="time-container"></span>
+// dynamic age script
 
-    const timeContainer = document.getElementById("time-container");
-    const BIRTH_DAY = "11-01-1996";
-    const BIRTH_DAY_DATE = new Date(BIRTH_DAY);
-    const intlNumberFormatter = new Intl.NumberFormat("en-US");
+//set timecontainer variable from html element
+//set my birthday and its conversion to date format
+//set interational number formatter variable
+const timeContainer = document.getElementById("age");
+const BIRTH_DAY = "11-01-1996";
+const BIRTH_DAY_DATE = new Date(BIRTH_DAY);
+const intlNumberFormatter = new Intl.NumberFormat("en-US");
 
-    setInterval(() => {
-      const now = new Date();
-      const difference = Math.floor(
-        (now.getTime() - BIRTH_DAY_DATE.getTime()) / 1000
-      );
 
-      timeContainer.innerText = intlNumberFormatter.format(difference);
-    }, 1000);
+// use interval function to print update every second
+// set now as current date and difference as the difference between current date and my birthday in seconds
+// round down and divided by 1000 to turn milliseconds into seconds
+// use division and modulus functions to find years, months, days, hours, minutes and seconds from  difference
+setInterval(() => {
+  const now = new Date();
+  const difference = Math.floor(
+    (now.getTime() - BIRTH_DAY_DATE.getTime()) / 1000
+  );
+
+  var numyears = Math.floor(difference / 31536000);
+  var nummonths = Math.floor((difference % 31536000) / 2628000);
+  var numdays = Math.floor(((difference % 31536000) % 2628000) / 86400);
+  var numhours = Math.floor((((difference % 31536000) % 2628000) % 86400)/3600);
+  var numminutes = Math.floor(((((difference % 31536000) % 2628000) % 86400) % 3600) / 60);
+  var numseconds = Math.floor(((((difference % 31536000) % 2628000) % 86400) % 3600) % 60);
   
+  // type out age
+  timeContainer.innerText = " - I am " + numyears + " years, " + nummonths + " months, " + numdays +  " days, " + numhours + " hours, " + numminutes + " minutes and " + numseconds + " seconds old"}, 1000);
+
+
+
+
+
