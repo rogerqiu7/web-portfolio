95e2f1ca77875de5f0facd209d7d4e3766123b3a
Roger Qiu <roger.qiu@coherehealth.com>
2023-08-09 13:13:42 -0500

    new webpage

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# My web portfolio - https://rogerqiu7.github.io/Web-Portfolio/
-## Description
-My webpage portfolio built using HTML, CSS and Javascript as well as the Splide library for the image carousel. The portfolio will contain many projects including Python, Javascript, Node.js and MySQL. You will be able to find source code for everything in my repositories or in the links at the bottom of all the pages. Inpiration for all my work comes from many sources such as Codecademy, The Odin Project and Youtube tutorials. 
+### Description
+My personal website and portfolio built using HTML, CSS and Javascript. The portfolio will contain many projects including Python, Javascript, and Java. You will be able to find source code for everything in my repositories or in the links at the bottom of all the pages.
---
+++ projects.js
@@ -0,0 +1,88 @@
+const projectsSection = document.getElementById('portfolio');
+
+const projects = [
+  {
+    img: {
+      src: './img/project-9-desktop.png',
+      alt: 'Car Rental preview',
+    },
+    title: 'Diabetes Prediction',
+    type: ['End to End Machine Learning Project', 'Data Science', 'Aug, 2023'],
+    description: 'Car Rental is a basic app that allows users to reserve a car, add new cars, and delete existing ones. The backend is built using Ruby On Rails and communicates with Reactjs app on the frontend to provide a seamless user experience. The app also includes functionality for user authentication and authorization, allowing only authorized users to perform actions such as adding or deleting cars.',
+    technologies: ['Ruby', 'Ruby On Rails', 'PostgreSQL', 'Reactjs', 'Redux'],
+    source: 'https://rogerqiu7.github.io/Simple-Calculator/',
+  },
+  {
+    img: {
+      src: './img/project-3-desktop.jpg',
+      alt: 'League Challengers tracker preview',
+    },
+    title: 'League Challengers Tracker',
+    type: ['Mobile', 'FRONTEND DEV', '2023'],
+    description: 'League Challengers Tracker is a basic application that aims to provide users with access to real-time statistics from Riot Games for the top challenger players in League of Legends. With this project, you can retrieve stats such as champion scores, KDA, and win rate for recent games played by these players, across all regions. You can also filter players to find the ones that best meet your needs.',
+    technologies: ['Reactjs', 'SCSS', 'Javascript', 'Redux', 'Jest'],
+    source: 'https://github.com/Kweeka1/metrics-app',
+  },
+  {
+    img: {
+      src: './img/project-4-desktop.png',
+      alt: 'Series Guide Application preview',
+    },
+    title: 'Series Guide Application',
+    type: ['Desktop', 'FRONTEND Dev', '2022'],
+    description: 'Series Guide Application is a small application that displays some of the popular shows in the world. It also allows users to share their comments and feedback about their favored shows and give a thumbs up thanks to the external Involvement and TVMaze API services.',
+    technologies: ['HTML', 'SCSS', 'Javascript', 'Webpack', 'Jest'],
+    source: 'https://github.com/Kweeka1/movies-app',
+  },
+  {
+    img: {
+      src: './img/project-6-desktop.png',
+      alt: '2022 Global artificial intelligence conference page preview',
+    },
+    title: 'Global AI Conference page',
+    type: ['WEB', 'FRONTEND DEV', '2022'],
+    description: 'This is my personel version of the conference page design by Cindy Shin in Behance. where I try to implement the mockup as precisely as possible. And I also changed the page content to talk about The Global AI Summit which is a two-way conversation that engages every attendee, speaker, and partner in a rich dialogue about how AI can solve complex problems, empower businesses, and transform society.',
+    technologies: ['HTML', 'CSS', 'Javascript'],
+    source: 'https://github.com/Kweeka1/capstone-project-conference-page',
+  },
+  {
+    img: {
+      src: './img/project-7-desktop.png',
+      alt: 'Math Magicians preview',
+    },
+    title: 'Math Magicians',
+    type: ['Desktop', 'FRONTEND DEV', '2022'],
+    description: 'Math Magicians is a simple calculator app that lets you perform basic arithmetic operations such as addition, subtraction, multiplication, and division. With an easy-to-use interface, you can quickly get the answers to all your mathematical problems with ease. ',
+    technologies: ['Reactjs', 'SCSS', 'Javascript', 'Jest'],
+    source: 'https://github.com/Kweeka1/math-magicians',
+  },
+];
+
+const generateList = (arr) => arr.reduce((elements, element) => `${elements}<li class="project_detail">${element}</li>`, '');
+
+const generateCard = (project, id) => `
+    <section class="card flex bg-white">
+      <img class="desktop-bg" src="${project.img.src}" alt="${project.img.alt}" />
+      <article>
+        <h3>${project.title}</h3>
+        <ul class="flex project_details">
+          ${generateList(project.type)}
+        </ul>
+        <p class="project__description">${project.description}</p>
+        <ul class="flex project_coding_langs">
+          ${generateList(project.technologies)}
+        </ul>
+        <div class="btn-container">
+          <a href="${project.source}" target="_blank">
+          <button id=${id} class="project-detail btn-primary btn-outlined">See Project</button>
+          </a>
+        </div>
+      </article>
+    </section>`;
+
+window.addEventListener('load', () => {
+  projects.forEach((project, index) => {
+    const card = generateCard(project, index);
+    projectsSection.insertAdjacentHTML('beforeend', card);
+  });
+});
